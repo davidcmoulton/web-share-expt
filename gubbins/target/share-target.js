a7bcc579eeb1ad6a8585b88e02364065de9f0da3
David Moulton <d.moulton@elifesciences.org>
2021-10-18 16:14:34 +0100

    Refactor gubbins for greater expressiveness

--- gubbins/target/share-target.js
+++ gubbins/target/share-target.js
@@ -1,24 +1,23 @@
 (function (win) {
 
-  const render = (content) => {
+  const display = (content) => {
     const div = win.document.createElement('p');
     div.innerHTML = content;
     win.document.querySelector('body').appendChild(div);
   };
 
-  const getParams = (location) => location.search.substring(1).split('&');
+  const getParams = (location) => (
+    location.search.substring(1).split('&').map((param) => param.split('='))
+  );
 
-  const displayAllowedFieldsFromParams = (params, allowedFields) => {
-    const availableAllowedFieldsWithValues = params
-      .filter((param) => allowedFields.includes(param.split('=')[0]))
-      .map((param) => `<p><b>${param.split('=')[0]}</b>: ${decodeURIComponent(param.split('=')[1])}</p>`)
-      .reduce((accumulator, currentValue) => accumulator + currentValue, '');
-    render(availableAllowedFieldsWithValues);
-  };
+  const extractAllowedFieldsFromParams = (params, allowedFieldNames) => (params
+    .filter((param) => allowedFieldNames.includes(param[0]))
+    .map((param) => `<p><b>${param[0]}</b>: ${decodeURIComponent(param[1])}</p>`)
+    .reduce((acc, current) => acc + current, '')
+  );
 
-  const displayUrl = (url) => render(`The URL is: <b>${url}</b>`);
-
-  displayAllowedFieldsFromParams(getParams(win.location), ['title', 'text', 'url']);
-  displayUrl(win.location);
+  const fields = extractAllowedFieldsFromParams(getParams(win.location), ['title', 'text', 'url']);
+  display(fields);
+  display(`The URL is: <b>${win.location}</b>`);
 
 })(window);
