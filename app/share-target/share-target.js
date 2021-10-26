c6bdbbe3426ca1538e4bb7a7f52631cbe0f719eb
David Moulton <d.moulton@elifesciences.org>
2021-10-26 15:03:48 +0100

    Confirmation screen with title param from url

--- app/share-target/share-target.js
+++ app/share-target/share-target.js
@@ -1,13 +1,50 @@
 (function (win) {
 
-  const render = (content) => {
-    const div = win.document.createElement('p');
-    div.innerHTML = content;
-    win.document.querySelector('body').appendChild(div);
+  const createElement = (elementName, content, classes) => {
+    const element = win.document.createElement(elementName);
+    if (content) {
+      element.innerHTML = content;
+    }
+    if (classes) {
+      element.classList.add(classes);
+    }
+    return element;
   };
 
-  const displayUrl = (url) => render(`The URL is: <b>${url}</b>`);
+  const getParams = (location) => (
+    location.search.substring(1).split('&').map((param) => param.split('='))
+  );
 
-  displayUrl(win.location);
+  const extractFieldsFromParams = (params, allowedFieldNames) => (params
+      .filter((param) => allowedFieldNames.includes(param[0]))
+      .map((param) => {
+        const obj = {};
+        obj[param[0]] = decodeURIComponent(param[1]);
+        return obj;
+      })
+  );
+
+  const getTitleFromUrl = (location) => {
+    const field = 'title';
+    const title = extractFieldsFromParams(getParams(location), field)[0][field];
+    console.log(title);
+    return title;
+  };
+
+  const showSucess = () => {
+    const img = createElement('img', null, 'message__image');
+    img.src = './images/icon-192.png';
+    img.alt = '';
+
+    const text = createElement('p', `Added <b>${getTitleFromUrl(win.location)}</b> to your list.`)
+
+    const messageEl = createElement('div', '', 'message');
+    messageEl.appendChild(text);
+    messageEl.appendChild(img);
+    win.document.querySelector('body').appendChild(messageEl)
+  };
+
+  getTitleFromUrl(win.location);
+  showSucess();
 
 })(window);
